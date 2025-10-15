e1e0787d90b1faeac3fcdc0c13550577ca6042cb
dlwlgusdev <dlwlgusdev@gmail.com>
2025-10-15 15:52:01 +0900

    feat(game): 연구 환경과 동일하게 진행하기 위해 목숨 1로 변경

--- src/const.py
+++ src/const.py
@@ -1,4 +1,3 @@
-
 from enum import Enum, IntEnum, auto
 
 APP_VERSION = "1.0"
@@ -10,25 +9,29 @@ APP_DISPLAY_SCALE = 2
 APP_CAPTURE_SCALE = 2
 APP_GFX_FILE = "gfx.png"
 
+
 class StageNum(IntEnum):
-    STAGE_1 = auto() # = 1
-    STAGE_2 = auto() # vortex
+    STAGE_1 = auto()  # = 1
+    STAGE_2 = auto()  # vortex
     STAGE_3 = auto()
-    STAGE_4 = auto() # vortex
+    STAGE_4 = auto()  # vortex
     STAGE_5 = auto()
+
+
 FINAL_STAGE = StageNum.STAGE_5
 
 STAGE_MUSIC_FILES = {
-    StageNum.STAGE_1 : "music_stage_1.json",
-    StageNum.STAGE_2 : "music_vortex.json",
-    StageNum.STAGE_3 : "music_stage_3.json",
-    StageNum.STAGE_4 : "music_vortex.json",
-    StageNum.STAGE_5 : "music_stage_5.json",
+    StageNum.STAGE_1: "music_stage_1.json",
+    StageNum.STAGE_2: "music_vortex.json",
+    StageNum.STAGE_3: "music_stage_3.json",
+    StageNum.STAGE_4: "music_vortex.json",
+    StageNum.STAGE_5: "music_stage_5.json",
 }
 MUSIC_GAME_COMPLETE = "music_game_complete.json"
 MUSIC_GAME_OVER = "music_game_over.json"
 MUSIC_BOSS = "music_boss.json"
 MUSIC_STAGE_CLEAR = "music_stage_clear.json"
+
 
 class EntityType(Enum):
     PLAYER = 0
@@ -38,8 +41,9 @@ class EntityType(Enum):
     POWERUP = auto()
     BACKGROUND = auto()
 
-STARTING_LIVES = 3
-MAX_LIVES = 9
+
+STARTING_LIVES = 1
+MAX_LIVES = 1
 MAX_WEAPONS = 3
 MAX_WEAPON_LEVEL = 5
 WEAPON_NAMES = ["A", "B", "C"]
@@ -55,25 +59,22 @@ PI = 3.141
 
 MAX_COLOURS = 16
 PALETTE = [
-    0xFF00FF, # transparent
+    0xFF00FF,  # transparent
     0x000000,
-    0x21c842,
-    0x5edc78,
-
-    0x5455ed,
-    0x7d76fc,
-    0xd4524d,
-    0x42ebf5,
-
-    0xfc5554,
-    0xff7978,
-    0xd4c154,
-    0xe6ce80,
-
-    0x21b03b,
-    0xc95bba,
-    0xcccccc,
-    0xffffff
+    0x21C842,
+    0x5EDC78,
+    0x5455ED,
+    0x7D76FC,
+    0xD4524D,
+    0x42EBF5,
+    0xFC5554,
+    0xFF7978,
+    0xD4C154,
+    0xE6CE80,
+    0x21B03B,
+    0xC95BBA,
+    0xCCCCCC,
+    0xFFFFFF,
 ]
 
 SOUNDS_RES_FILE = "sounds.pyxres"
